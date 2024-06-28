15c664e3a4efffcdde1b80643df10c0951477516
Magomeds05 <yusypovm@gmail.com>
2024-06-28 14:47:55 +0300

    Update module3.1.py

--- module2.3.py
+++ module2.3.py
@@ -1,24 +1,30 @@
 call = 0
 
-def count_calls():
-    global call
+def count_calls(): #подсчет вызовов остальных функций.
+    global call #глобальная переменная
     call += 1
-
-count_calls()
+    return call
 
 def string_info(string):
-    global call
-    call = 5
-    print(string)
-    print(call)
-
-string_info(15)
-string_info('Fifteen')
+    count_calls()
+    a = len(string) #длины этой строки
+    b = string.upper() #строку в верхнем регистре
+    c = string.lower() #строку в нижнем регистре.
+    return a, b, c
 
 def is_contains(string, list_to_search):
-    print(string, list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if string.lower() in list_to_search[i].lower() or list_to_search[i].lower() in string.lower():
+            return True
+        else:
+            return False
 
-is_contains(True, False)
-is_contains('Str', 'Str2')
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # если строка находится в этом списке
+print(is_contains('cycle', ['recycling', 'cyclic'])) # если отсутствует
+print(call)
+
 
 
